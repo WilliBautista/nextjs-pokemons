434c74a26e2cbefe15c5a21af555156c480173bd
William Bautista <william.bautista@projekt202.com>
2022-08-15 12:46:55 -0500

    Enable Incremental Static Regeneration (ISR) and Incremental Statig Generation (ISG)

--- utils/getPokemonData.ts
+++ utils/getPokemonData.ts
@@ -2,12 +2,29 @@ import { pokeApi } from "../api"
 import { Pokemon } from "../interfaces"
 
 export const getPokemonData = async ( nameOrId: string ) => {
-  const { data } = await pokeApi.get<Pokemon>(`/pokemon/${ nameOrId }`)
-  const { id, name, sprites } = data
 
-  return {
-    id,
-    name,
-    sprites
+  try {
+    const { data } = await pokeApi.get<Pokemon>(`/pokemon/${ nameOrId }`)
+    const { id, name, sprites } = data
+
+
+
+    return {
+      id,
+      name,
+      sprites: {
+        front_default: sprites.front_default,
+        back_default: sprites.back_default,
+        front_shiny: sprites.front_shiny,
+        back_shiny: sprites.back_shiny,
+        other: {
+          dream_world: {
+            front_default: sprites.other?.dream_world.front_default
+          }
+        }
+      }
+    }
+  } catch (error) {
+    return null;
   }
 }
